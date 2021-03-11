a27700a3ccbc0a74fa1ed1e619b762a2b836625e
Bluenarysun <tee.emilond@gmail.com>
2021-03-12 00:50:01 +0700

    init cypress

---
+++ cypress/integration/e2e_tests/search.spec.js
@@ -0,0 +1,13 @@
+describe('Search Box Test', () => {
+  before(() => {
+    cy.visit('http://zero.webappsecurity.com/index.html');
+  });
+
+  it('should type into searchbox and submit with pressing enter', () => {
+    cy.get('#searchTerm').type('some text {enter}');
+  });
+
+  it('should show search results page', () => {
+    cy.get('h2').should('have.visible');
+  });
+});
