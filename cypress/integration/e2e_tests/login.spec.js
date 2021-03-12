c9f13ff507b4fe2a5716b6adcad4b53d576ad507
Bluenarysun <tee.emilond@gmail.com>
2021-03-12 15:16:00 +0700

    Tip to use fixture and common.js

--- cypress/integration/e2e_tests/login.spec.js
+++ cypress/integration/e2e_tests/login.spec.js
@@ -21,10 +21,7 @@ describe('Login / Logout Test', () => {
       const username = user.id;
       const password = user.pwd;
 
-      cy.get('#user_login').type(username);
-      cy.get('#user_password').type(password);
-      cy.get('#user_remember_me').click();
-      cy.contains('Sign in').click();
+      cy.login(username, password);
     });
   });
 
